c78fccf5571e016c101f2e3518e9fd5759437e6f
lcriado <lcriado@mbmed.com>
2024-11-23 14:01:37 -0300

    Primer commit, se agrega el data grid view y el picture box

---
+++ eCommerceApp/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace eCommerceApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvArticles = new System.Windows.Forms.DataGridView();
+            this.picBoxArticle = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticles)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxArticle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvArticles
+            // 
+            this.dgvArticles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticles.Location = new System.Drawing.Point(12, 12);
+            this.dgvArticles.Name = "dgvArticles";
+            this.dgvArticles.RowHeadersWidth = 62;
+            this.dgvArticles.RowTemplate.Height = 28;
+            this.dgvArticles.Size = new System.Drawing.Size(735, 430);
+            this.dgvArticles.TabIndex = 0;
+            // 
+            // picBoxArticle
+            // 
+            this.picBoxArticle.Location = new System.Drawing.Point(754, 13);
+            this.picBoxArticle.Name = "picBoxArticle";
+            this.picBoxArticle.Size = new System.Drawing.Size(496, 429);
+            this.picBoxArticle.TabIndex = 1;
+            this.picBoxArticle.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 454);
+            this.Controls.Add(this.picBoxArticle);
+            this.Controls.Add(this.dgvArticles);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticles)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxArticle)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvArticles;
+        private System.Windows.Forms.PictureBox picBoxArticle;
+    }
+}
+
